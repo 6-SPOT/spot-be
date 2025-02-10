4d60d3f566299ac2c25525fb9ceb73a07f2b3a22
dalcheonroadhead <wjsaos2081@gmail.com>
2025-02-10 17:24:43 +0900

    FEAT: 공통 응답 및 Error 반환 로직 완성

---
+++ src/main/java/spot/spot/global/config/GlobalResponseHandler.java
@@ -0,0 +1,46 @@
+package spot.spot.global.config;
+
+import org.springframework.core.MethodParameter;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+import spot.spot.global.dto.response.ResultResponse;
+import spot.spot.global.exception.ErrorCode;
+import spot.spot.global.exception.GlobalException;
+
+@RestControllerAdvice
+public class GlobalResponseHandler implements ResponseBodyAdvice<Object> {
+
+    // 해당 Advice 적용 범위
+    @Override
+    public boolean supports(MethodParameter returnType,
+        Class<? extends HttpMessageConverter<?>> converterType) {
+        return true;
+    }
+
+    // 응답 변환 매서드
+    @Override
+    public Object beforeBodyWrite(Object body, MethodParameter returnType,
+        MediaType selectedContentType,
+        Class<? extends HttpMessageConverter<?>> selectedConverterType, ServerHttpRequest request,
+        ServerHttpResponse response) {
+
+        // 만약 반환 타입이 void이면 data 없이 응답
+        if (Void.TYPE.equals(returnType.getParameterType())) {
+            return new ResultResponse("success", "처리가 완료되었습니다.", null);
+        }
+
+        // 만약 String이면 예외 발생
+        if (body instanceof String) {
+            throw new GlobalException(ErrorCode.NOT_ALLOW_STRING);
+        }
+
+        if(body instanceof ResultResponse){
+            return body;
+        }
+        return ResultResponse.success(body);
+    }
+}
---
+++ src/main/java/spot/spot/global/exception/GlobalException.java
@@ -0,0 +1,10 @@
+package spot.spot.global.exception;
+
+import lombok.Getter;
+
+@Getter
+public class GlobalException extends RuntimeException{
+    private final ErrorCode errorCode;
+
+    public GlobalException(ErrorCode errorCode) { this.errorCode = errorCode; }
+}
