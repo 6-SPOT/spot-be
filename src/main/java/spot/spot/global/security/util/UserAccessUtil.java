c895ea3122d5c9d91b971fa30fe2b6300af3114c
dalcheonroadhead <wjsaos2081@gmail.com>
2025-02-16 18:18:09 +0900

    FEAT : TEST USER DATA 삽입 및 재활용 로직 작성

---
+++ src/main/java/spot/spot/global/security/util/UserAccessUtil.java
@@ -0,0 +1,22 @@
+package spot.spot.global.security.util;
+
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
+import org.springframework.stereotype.Component;
+import spot.spot.domain.member.entity.Member;
+import spot.spot.domain.member.repository.MemberRepository;
+
+@Component
+@RequiredArgsConstructor
+public class UserAccessUtil {
+
+    private final MemberRepository memberRepository;
+
+    public Optional<Member> geetMember() {
+//        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+//        String userName = authentication.getName();
+//        return memberRepository.findByName(userName);
+        return memberRepository.findById(-1L);
+    }
+}
