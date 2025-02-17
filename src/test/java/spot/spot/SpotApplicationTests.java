b5cf4c928c132a0f4c9bb41925285fa1b6ce27b0
hxcva1 <hxcva1@gmail.com>
2025-02-17 17:10:04 +0900

    chore: test 환경변수 추가

--- src/test/java/spot/spot/SpotApplicationTests.java
+++ src/test/java/spot/spot/SpotApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest(properties = "spring.data.redis.enabled=false")
+@SpringBootTest
 class SpotApplicationTests {
 
 	@Test
