c5ce44e61ea13b62b4a4323d73e86241424212c7
dalcheonroadhead <wjsaos2081@gmail.com>
2025-02-18 19:16:55 +0900

    CHORE: REBASE #52 ONTO DEVELOP

--- src/main/java/spot/spot/domain/member/repository/MemberRepository.java
+++ src/main/java/spot/spot/domain/member/repository/MemberRepository.java
@@ -1,13 +1,28 @@
 package spot.spot.domain.member.repository;
 
+import java.util.List;
+import java.util.Optional;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import spot.spot.domain.member.entity.Member;
 
-import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
     boolean existsById(@NotNull Long id);
+
+    @Query(value = """
+    SELECT  m.*,
+            (6371*acos(
+                    cos(radians(:lat)) * cos(radians(m.lat))
+                    * cos(radians(m.lng) - radians(:lng)) + sin(radians(:lat)) * sin(radians(m.lat)))) AS distance
+    FROM Members m
+    INNER JOIN Worker w ON m.id = w.member_id
+    HAVING distance < :dist
+    ORDER BY distance
+    """, nativeQuery = true)
+    List<Member> findWorkersNearByMember(@Param("lat") double lat, @Param("lng") double lng, @Param("dist") double dist);
 
     Optional<Member> findByNickname(String nickname);
 }
