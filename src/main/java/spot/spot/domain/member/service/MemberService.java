9ef4cd9434d2077570acab35639994aafcaf856c
immyeong <wpfls6670@naver.com>
2025-02-23 12:50:20 +0900

    feat: 멤버 정보 수정 api

--- src/main/java/spot/spot/domain/member/service/MemberService.java
+++ src/main/java/spot/spot/domain/member/service/MemberService.java
@@ -2,12 +2,12 @@ package spot.spot.domain.member.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import spot.spot.domain.member.entity.Member;
 import spot.spot.domain.member.entity.dto.MemberRequest;
 import spot.spot.domain.member.entity.dto.MemberRole;
+import spot.spot.domain.member.repository.MemberQueryRepository;
 import spot.spot.domain.member.repository.MemberRepository;
 
 import java.util.Optional;
@@ -15,11 +15,12 @@ import java.util.Optional;
 @Service
 @RequiredArgsConstructor
 @Slf4j
+@Transactional
 public class MemberService {
 
     private final MemberRepository memberRepository;
+    private final MemberQueryRepository memberQueryRepository;
 
-    @Transactional
     public void register(MemberRequest.register register) {
 
         Member member = Member.builder()
@@ -32,7 +33,6 @@ public class MemberService {
         memberRepository.save(member);
     }
 
-    @Transactional
     public Member findByNickname(String nickname) {
         Optional<Member> findMember = memberRepository.findByNickname(nickname);
         if(findMember.isEmpty()) return null;
@@ -40,11 +40,15 @@ public class MemberService {
         return findMember.get();
     }
 
-    @Transactional
     public Member findById(Long id) {
         Optional<Member> findMember = memberRepository.findById(id);
         if(findMember.isEmpty()) return null;
 
         return findMember.get();
+    }
+
+    public void modify(MemberRequest.modify modify, String memberId) {
+        Long parseMemberId = Long.parseLong(memberId);
+        memberQueryRepository.updateMember(parseMemberId, modify);
     }
 }
