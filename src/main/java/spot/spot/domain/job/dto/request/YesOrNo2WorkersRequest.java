947435be7ab0270f32961e75187aa2cde1ed9b9b
dalcheonroadhead <wjsaos2081@gmail.com>
2025-02-24 16:06:43 +0900

    DOCS: DTO 이름 직관적으로 수정

--- src/main/java/spot/spot/domain/job/dto/request/YesOrNo2WorkersRequest.java
+++ src/main/java/spot/spot/domain/job/dto/request/YesOrNo2WorkersRequest.java
@@ -2,10 +2,10 @@ package spot.spot.domain.job.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 
-public record YesOrNoOfClientRequest(
+public record YesOrNo2WorkersRequest(
     @Schema(description = "해결사가 맡으면 하는 일의 아이디")
     long jobId,
-    @Schema(description = "해결사 dkdlel")
+    @Schema(description = "해결사 아이디")
     long attenderId,
     @Schema(description = "true = YES, false = NO")
     boolean isYes
