5d9c8a8723f0416deebc5044b2d5ca8263147e63
dalcheonroadhead <wjsaos2081@gmail.com>
2025-03-04 02:06:20 +0900

    FEAT: STOMP JWT 검증 로직 추가

--- src/main/java/spot/spot/global/stomp/StompHandler.java
+++ src/main/java/spot/spot/global/stomp/StompHandler.java
@@ -1,7 +1,14 @@
 package spot.spot.global.stomp;
 
+import static spot.spot.global.util.ConstantUtil.PERMIT_ALL;
+
+import java.util.Objects;
+import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.MessagingException;
 import org.springframework.messaging.simp.stomp.StompCommand;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.messaging.support.ChannelInterceptor;
@@ -9,16 +16,28 @@ import org.springframework.stereotype.Component;
 
 import lombok.RequiredArgsConstructor;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class StompHandler implements ChannelInterceptor {
 
-	@Override
-	public Message<?> preSend(Message<?> message, MessageChannel channel) {
-		final StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
+	private final StompUtil stompUtil;
 
-		if(accessor.getCommand() == StompCommand.CONNECT) {
-			// 연결 시도 경우 토큰 검증
+	@NonNull
+	@Override
+	public Message<?> preSend(@NonNull Message<?> message, @NonNull MessageChannel channel) {
+		final StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
+		log.info("STOMP 접근 명령어={}", accessor.getCommand());
+		String atk = stompUtil.getAccessToken(accessor);
+		if(atk == null) throw new MessagingException("JWT TOKEN is NULL");
+		switch (accessor.getCommand()) {
+			case CONNECT :
+				String msgType = accessor.getFirstNativeHeader("type");
+				if(Objects.equals(msgType,PERMIT_ALL)) {break;}
+			case SUBSCRIBE:
+			case SEND:
+				stompUtil.verifyAccessToken(atk);
+				break;
 		}
 
 		return message;
---
+++ src/main/java/spot/spot/global/stomp/StompUtil.java
@@ -0,0 +1,40 @@
+package spot.spot.global.stomp;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessagingException;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.stereotype.Component;
+import spot.spot.global.response.format.ErrorCode;
+import spot.spot.global.security.util.JwtUtil;
+import static spot.spot.global.util.ConstantUtil.AUTH_ERROR;
+
+@Component
+@RequiredArgsConstructor
+public class StompUtil {
+
+    private final JwtUtil jwtUtil;
+
+    public String getAccessToken(StompHeaderAccessor accessor) {
+        return accessor.getFirstNativeHeader(AUTH_ERROR);
+    }
+
+    public void verifyAccessToken(String token) {
+        ErrorCode errorCode = jwtUtil.validateToken(token);
+        if(errorCode != null) {
+            throw  new MessagingException(errorCode.getMessage());
+        }
+    }
+
+    public Message<?> handleErrorMessage(StompHeaderAccessor accessor, String errorMessage) {
+        accessor.setLeaveMutable(true);
+        accessor.setHeader(AUTH_ERROR, AUTH_ERROR);
+
+        return MessageBuilder.withPayload(errorMessage)
+            .setHeaders(accessor)
+            .build();
+    }
+
+
+}
