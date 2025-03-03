28371a09c4279388cec3b35797fb63fd4df00fea
dalcheonroadhead <wjsaos2081@gmail.com>
2025-03-04 02:06:45 +0900

    FEAT: STOMP 비정상 종료 세션 추적

--- src/main/java/spot/spot/global/stomp/StompEventListener.java
+++ src/main/java/spot/spot/global/stomp/StompEventListener.java
@@ -1,21 +1,37 @@
 package spot.spot.global.stomp;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
+import org.springframework.messaging.Message;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Component;
+import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.messaging.SessionConnectEvent;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
+@Slf4j
 @Component
+@RequiredArgsConstructor
 public class StompEventListener {
+
+	private final ActiveSessionTracker sessionTracker;
 
 	@EventListener
 	public void connectionHandle(SessionConnectEvent event) {
-		// 연결 된 경우
+		StompHeaderAccessor accessor = StompHeaderAccessor.wrap(event.getMessage());
+		String sessionId = accessor.getSessionId();
+		sessionTracker.addSession(sessionId);
 	}
 
 	@EventListener
 	public void disconnectionHandle(SessionDisconnectEvent event) {
-		// 연결을 끊는 경우
+		StompHeaderAccessor accessor = StompHeaderAccessor.wrap(event.getMessage());
+		String sessionId = accessor.getSessionId();
+
+		if(sessionTracker.isSessionActive(sessionId)){
+			log.warn("비정상 종료 감지!, 세션 ID: {}", sessionId);
+		}
+		sessionTracker.removeSession(sessionId);
 	}
 }
