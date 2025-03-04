bdcca78e619e66c4749bd8e46952cb6702c6625a
dalcheonroadhead <wjsaos2081@gmail.com>
2025-03-04 10:26:07 +0900

    FEAT: 실시간 위치 확인 SOCKET 완성

---
+++ src/main/java/spot/spot/domain/job/mapper/JobStreamingMapper.java
@@ -0,0 +1,21 @@
+package spot.spot.domain.job.mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.ReportingPolicy;
+import spot.spot.domain.job.dto.request.PositionRequest;
+import spot.spot.domain.job.dto.response.PositionResponse;
+import spot.spot.domain.member.entity.Member;
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface JobStreamingMapper {
+
+    @Mapping(source = "positionRequest.jobId", target = "jobId")
+    @Mapping(source = "positionRequest.lat", target = "lat")
+    @Mapping(source = "positionRequest.lng", target = "lng")
+    @Mapping(source = "member.id", target = "senderId")
+    @Mapping(source = "member.nickname", target = "senderName")
+    @Mapping(source = "member.img", target = "senderImg")
+    @Mapping(target = "sendDateTime", expression = "java(System.currentTimeMillis())")
+    PositionResponse toPositionResponse(Member member, PositionRequest positionRequest);
+}
