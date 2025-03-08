0d0c19265ab7b440227f6129d4f38b6ce60dfde3
immyeong <wpfls6670@naver.com>
2025-03-08 20:18:00 +0900

    refactor: 포인트 등록 동시성 락 문제

--- src/main/java/spot/spot/domain/member/repository/MemberRepository.java
+++ src/main/java/spot/spot/domain/member/repository/MemberRepository.java
@@ -1,14 +1,9 @@
 package spot.spot.domain.member.repository;
 
-import java.util.List;
 import java.util.Optional;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import spot.spot.domain.member.entity.Member;
-
-import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
     boolean existsById(@NotNull Long id);
