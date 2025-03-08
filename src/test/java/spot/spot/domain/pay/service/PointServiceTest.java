003972f4409ddd67d82fa485517ef4a2c4517858
immyeong <wpfls6670@naver.com>
2025-03-08 20:17:37 +0900

    test: 포인트 등록 동시성 테스트

--- src/test/java/spot/spot/domain/pay/service/PointServiceTest.java
+++ src/test/java/spot/spot/domain/pay/service/PointServiceTest.java
@@ -1,14 +1,15 @@
  package spot.spot.domain.pay.service;
 
  import lombok.extern.slf4j.Slf4j;
- import org.assertj.core.api.Assertions;
+ import org.junit.jupiter.api.AfterEach;
  import org.junit.jupiter.api.BeforeEach;
  import org.junit.jupiter.api.DisplayName;
  import org.junit.jupiter.api.Test;
+ import org.junit.jupiter.params.ParameterizedTest;
+ import org.junit.jupiter.params.provider.ValueSource;
  import org.springframework.beans.factory.annotation.Autowired;
  import org.springframework.boot.test.context.SpringBootTest;
  import org.springframework.test.context.ActiveProfiles;
- import org.springframework.transaction.annotation.Transactional;
  import spot.spot.domain.member.dto.request.MemberRequest;
  import spot.spot.domain.member.entity.Member;
  import spot.spot.domain.member.repository.MemberRepository;
@@ -20,9 +21,14 @@
 
  import java.util.ArrayList;
  import java.util.List;
+ import java.util.concurrent.CountDownLatch;
+ import java.util.concurrent.ExecutorService;
+ import java.util.concurrent.Executors;
+ import java.util.concurrent.atomic.AtomicInteger;
+
+ import static org.assertj.core.api.Assertions.assertThat;
 
  @SpringBootTest
- @Transactional
  @ActiveProfiles("local")
  @Slf4j
  class PointServiceTest {
@@ -62,6 +68,12 @@
          responseDtos = pointService.servePoint(serveRequestDtos);
      }
 
+     @AfterEach
+     void after() {
+         memberRepository.deleteAllInBatch();
+         pointRepository.deleteAllInBatch();
+     }
+
      @Test
      @DisplayName("포인트 생성 시 포인트의 갯수만큼 사용할 수 있는 포인트가 저장된다.")
      void servePoint() {
@@ -73,7 +85,7 @@
          }
 
          List<Point> all = pointRepository.findAll();
-         Assertions.assertThat(all.size()).isEqualTo(8);
+         assertThat(all.size()).isEqualTo(8);
      }
 
      @Test
@@ -89,8 +101,25 @@
              List<Point> afterByPointCodeAndIsValidTrue = pointRepository.findByPointCodeAndIsValidTrue(pointCode);
              int afterRegisterPoint = afterByPointCodeAndIsValidTrue.size();
 
-             Assertions.assertThat(beforePointCount).isEqualTo(afterRegisterPoint + 1);
+             assertThat(beforePointCount).isEqualTo(afterRegisterPoint + 1);
          }
+     }
+
+     @Test
+     @DisplayName("포인트 등록이 성공하면 유저의 포인트가 증가한다.")
+     void registerPointWithMemberPoint() {
+         ///given
+         Member findMember = memberService.findByNickname("테스트유저1");
+
+         ///when
+         for (int i = 0; i < responseDtos.size(); i++) {
+             String pointCode = responseDtos.get(i).pointCode();
+             pointRepository.findByPointCodeAndIsValidTrue(pointCode);
+             pointService.registerPoint(pointCode, String.valueOf(findMember.getId()));
+         }
+
+         ///then
+         assertThat(memberService.findByNickname("테스트유저1").getPoint()).isEqualTo(2000);
      }
 
      @Test
@@ -104,11 +133,11 @@
              List<Point> afterByPointCodeAndIsValidTrue = pointRepository.findByPointCodeAndIsValidTrue(pointCode);
              int afterRegisterPoint = afterByPointCodeAndIsValidTrue.size();
 
-             Assertions.assertThat(beforePointCount).isEqualTo(afterRegisterPoint + 1);
+             assertThat(beforePointCount).isEqualTo(afterRegisterPoint + 1);
          }
 
          List<Point> all = pointRepository.findAll();
-         Assertions.assertThat(all.size()).isEqualTo(6);
+         assertThat(all.size()).isEqualTo(6);
      }
 
      @Test
@@ -120,11 +149,50 @@
              List<Point> afterByPointCodeAndIsValidTrue = pointRepository.findByPointCodeAndIsValidTrue(pointCode);
              int afterRegisterPoint = afterByPointCodeAndIsValidTrue.size();
 
-             Assertions.assertThat(afterRegisterPoint).isEqualTo(0);
+             assertThat(afterRegisterPoint).isEqualTo(0);
          }
 
          List<Point> all = pointRepository.findAll();
 
-         Assertions.assertThat(all.size()).isEqualTo(0);
+         assertThat(all.size()).isEqualTo(0);
+     }
+
+     @DisplayName("포인트가 개수가 5개인 포인트를 동시에 5, 10, 15 개의 쓰레드가 등록하려고 하면 0개, 5개, 10 개의 쓰레드가 실패한다.")
+     @ParameterizedTest
+     @ValueSource(ints = {5, 10, 15})
+     void registerPointMultiThread(int threads) throws InterruptedException {
+         ///given
+         Member findMember = memberService.findByNickname("테스트유저1");
+         CountDownLatch doneSignal = new CountDownLatch(threads);
+         ExecutorService executorService = Executors.newFixedThreadPool(threads);
+
+         AtomicInteger successCount = new AtomicInteger(0);
+         AtomicInteger failCount = new AtomicInteger(0);
+         String pointCode = responseDtos.get(1).pointCode();
+
+         /// when
+         for (int i = 0; i < threads; i++) {
+             int finalI = i;
+             executorService.execute(() -> {
+                 try {
+                     log.info("{} 번째 쓰레드 접근 시작", finalI);
+                     pointService.registerPoint(pointCode, String.valueOf(findMember.getId()));
+                     successCount.getAndIncrement();
+                 } catch (Exception e) {
+                     failCount.getAndIncrement();
+                 } finally {
+                     log.info("{} 번째 쓰레드 접근 종료", finalI);
+                     doneSignal.countDown();
+                 }
+             });
+         }
+
+         /// 스레드 종료 대기
+         doneSignal.await();
+         executorService.shutdown();
+
+         /// then
+         int resultFailThreads = threads - 5;
+         assertThat(resultFailThreads).isEqualTo(failCount.get());
      }
  }
