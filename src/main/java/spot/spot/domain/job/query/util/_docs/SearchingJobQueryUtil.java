9e893bde63da59ea78bc6ddbb39a618a97d0990f
dalcheonroadhead <wjsaos2081@gmail.com>
2025-03-11 13:37:16 +0900

    REFACTOR: Searching 명세 분리

--- src/main/java/spot/spot/domain/job/query/util/_docs/SearchingJobQueryUtil.java
+++ src/main/java/spot/spot/domain/job/query/util/_docs/SearchingJobQueryUtil.java
@@ -7,5 +7,6 @@ import spot.spot.domain.job.query.dto.response.NearByJobResponse;
 
 @Service
 public interface SearchingJobQueryUtil {
+    // 1. 근처 사용자 찾아서 페이지 네이션 적용
     Slice<NearByJobResponse> findNearByJobs (double lat, double lng, int zoom, Pageable pageable);
 }
