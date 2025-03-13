e4317b0e6316e0125916a71ea7bad390a5c39f88
dalcheonroadhead <wjsaos2081@gmail.com>
2025-03-13 13:06:54 +0900

    DOCS: SWAGGER 명세 추가

--- src/main/java/spot/spot/global/healthcheck/command/controller/_docs/HealthCheckCommandDocs.java
+++ src/main/java/spot/spot/global/healthcheck/command/controller/_docs/HealthCheckCommandDocs.java
@@ -4,8 +4,8 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.RequestBody;
 import spot.spot.global.healthcheck.command.dto.SampleDto;
 
-@Tag(name= "나. HEALTHCHECK COMMAND API")
+@Tag(name= "나. HEALTHCHECK COMMAND API", description = "<br/> Post Method Health Checking ")
 public interface HealthCheckCommandDocs {
-    @Operation(summary = "POST METHOD HEALTH CHECKING")
+    @Operation(summary = "POST METHOD 반환 상태 확인")
     public SampleDto postDto(@RequestBody SampleDto dto);
 }
