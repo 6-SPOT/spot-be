891b5dd516066d6cc9449d843a507dc043df8dd9
dalcheonroadhead <wjsaos2081@gmail.com>
2025-03-13 14:55:49 +0900

    FIX: fix dto grammar

--- src/main/java/spot/spot/domain/job/query/dto/response/NearByJobResponse.java
+++ src/main/java/spot/spot/domain/job/query/dto/response/NearByJobResponse.java
@@ -12,7 +12,7 @@ public class NearByJobResponse {
     @Schema(description = "일의 제목")
     private String title;
     @Schema(description = "일의 내용")
-    private String contnent;
+    private String content;
     @Schema(description = "일의 프로필 사진")
     private String picture;
     @Schema(description = "위도")
