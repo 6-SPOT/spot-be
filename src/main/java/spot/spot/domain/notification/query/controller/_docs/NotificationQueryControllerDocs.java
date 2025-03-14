b0602ee6c67a3286934ac52d3066f2f8829bfe3c
dalcheonroadhead <wjsaos2081@gmail.com>
2025-03-14 13:51:06 +0900

    FEAT: 알림 목록 최신 순으로 가져 오기

--- src/main/java/spot/spot/domain/notification/query/controller/_docs/NotificationQueryControllerDocs.java
+++ src/main/java/spot/spot/domain/notification/query/controller/_docs/NotificationQueryControllerDocs.java
@@ -6,7 +6,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import spot.spot.domain.notification.query.dto.response.NotificationResponse;
 
-@Tag(name="NOTIFICATION QUERY API", description = "<br/> 알림 조회 API")
+@Tag(name="6. NOTIFICATION QUERY API", description = "<br/> 알림 조회 API")
 public interface NotificationQueryControllerDocs {
     @Operation(summary = "내 알림 목록의 최신순을 가져오는 API", description = "page = 페이지 번호, size = 한 페이지 당 알림 개수")
     public Slice<NotificationResponse> getMyNotificationList(Pageable pageable);
