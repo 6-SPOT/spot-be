c46d32c412f1034ffaa5e093a6bb43e5d117487b
42kko <kko@student.42seoul.kr>
2025-03-28 14:40:34 +0900

    Fix: `-`와 슬래쉬가 잘못적혀있어 수정했습니다.

--- prod_revision.sh
+++ prod_revision.sh
@@ -39,7 +39,7 @@ echo "최신 patch 버전: $LATEST_PATCH"
 
 NEXT_PATCH=$((LATEST_PATCH + 1))
 NEW_VERSION="1.0.${NEXT_PATCH}"
-NEW_FILENAME="${SERVER_TYPE}-${NEW_VERSION}.zip"
+NEW_FILENAME="${SERVER_TYPE}/${NEW_VERSION}.zip"
 
 OLD_FILENAME="${SERVER_TYPE}/1.0.${LATEST_PATCH}.zip"
 unzip $OLD_FILENAME -d ./extracted
@@ -53,7 +53,7 @@ cd ..
 # S3 업로드 (버전별 + latest.zip)
 aws s3 cp $NEW_FILENAME ${S3_BUCKET_PROD}/${SERVER_TYPE}/
 cp $NEW_FILENAME latest.zip
-aws s3 cp latest.zip ${S3_BUCKET_PROD}${SERVER_TYPE}/
+aws s3 cp latest.zip ${S3_BUCKET_PROD}/${SERVER_TYPE}/
 
 # Github Actions용 환경변수 출력
 echo "NEW_VERSION=${NEW_VERSION}" >> $GITHUB_ENV
