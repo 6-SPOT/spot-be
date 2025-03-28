f71799ae1ab97f114f3fe3842d4e4a7a84f09b4e
42kko <kko@student.42seoul.kr>
2025-03-28 14:34:27 +0900

    Fix: 쉘스크립트 수정

--- prod_revision.sh
+++ prod_revision.sh
@@ -3,20 +3,39 @@
  SERVER_TYPE="be"
 # S3_BUCKET="your-test-bucket"
 
-PATCH_VERSIONS=$(aws s3 ls ${S3_BUCKET_PROD}/${SERVER_TYPE} | while read -r line; do
-  FILE=$(echo "$line" | xargs | cut -d' ' -f4)
+echo "S3 목록:"
+aws s3 ls ${S3_BUCKET_PROD}/${SERVER_TYPE}/
 
-  if [[ "$FILE" == *.zip ]]; then
-    # 예: fe-1.0.13.zip → 13 추출
-    VERSION_PART=${FILE%.zip}        # .zip 제거 → 1.0.13
-    PATCH=${VERSION_PART##*.}        # 마지막 . 기준 → 13
-    echo "$PATCH"
-  fi
-done)
+echo "============="
 
+# 최신 버전 목록 추출
+PATCH_VERSIONS=$(aws s3 ls ${S3_BUCKET_PROD}/${SERVER_TYPE}/ | \
+  awk '{print $NF}' | \
+  grep -E "^1\.0\.[0-9]+\.zip$" | \
+  sed -E "s/1\.0\.//; s/\.zip//")
+
+echo "버전 리스트:"
+echo "$PATCH_VERSIONS"
+
+# 최신 버전 구하기
 LATEST_PATCH=$(echo "$PATCH_VERSIONS" | sort -n | tail -n 1)
 
 echo "최신 patch 버전: $LATEST_PATCH"
+
+#PATCH_VERSIONS=$(aws s3 ls ${S3_BUCKET_PROD}/${SERVER_TYPE} | while read -r line; do
+#  FILE=$(echo "$line" | awk '{print $NF}')
+#
+#  if [[ "$FILE" == *.zip ]]; then
+#    # 예: fe-1.0.13.zip → 13 추출
+#    VERSION_PART=${FILE%.zip}        # .zip 제거 → 1.0.13
+#    PATCH=${VERSION_PART##*.}        # 마지막 . 기준 → 13
+#    echo "$PATCH"
+#  fi
+#done)
+#
+#LATEST_PATCH=$(echo "$PATCH_VERSIONS" | sort -n | tail -n 1)
+#
+#echo "최신 patch 버전: $LATEST_PATCH"
 
 NEXT_PATCH=$((LATEST_PATCH + 1))
 NEW_VERSION="1.0.${NEXT_PATCH}"
