c0bba9751a6bb7a574fe0331ec8c9d20df9c2d71
dalcheonroadhead <wjsaos2081@gmail.com>
2025-04-02 02:55:00 +0900

    FEAT: cache Config 만들기 + 의존성 추가

---
+++ src/main/java/spot/spot/global/config/CacheConfig.java
@@ -0,0 +1,23 @@
+package spot.spot.global.config;
+
+import com.github.benmanes.caffeine.cache.Caffeine;
+import java.time.Duration;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableCaching
+public class CacheConfig {
+    @Bean
+    public CacheManager cacheManager() {
+        CaffeineCacheManager manager = new CaffeineCacheManager("job-geohash");
+        manager.setCaffeine(Caffeine.newBuilder()
+            .maximumSize(10_000)
+            .expireAfterWrite(Duration.ofDays(1))
+            .recordStats());
+        return manager;
+    }
+}
